f2d7dad696ede6a4655024dc3cbb5b99d3c0e4f4
Ryan Underwood <egryan1@gmail.com>
2011-06-06 20:42:21 -0400

    end of 7.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name,  :presence => true,
@@ -19,4 +21,22 @@ class User < ActiveRecord::Base
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+  validates :password, :presense 	 => true,
+					   :confirmation => true,
+					   :length		 => { :within => 6..40}
+					   
+  before_save :encrypt_password
+  
+  private
+  
+	def encrypt_password
+		self.encrypted_password = encrypt(password)
+	end
+	
+	def encrypt(string)
+		string
+	end
+		
+  
+					
 end
